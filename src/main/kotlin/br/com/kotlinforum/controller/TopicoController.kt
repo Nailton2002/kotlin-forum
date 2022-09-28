36228bfd3f67c13a4fb55cf81cb92e0ec746963a
Nailton2002 <jnmp25jnmp@gmail.com>
2022-09-28 10:09:41 -0300

    Cadastrando topicos

--- src/main/kotlin/br/com/kotlinforum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/kotlinforum/controller/TopicoController.kt
@@ -6,6 +6,8 @@ import br.com.kotlinforum.modelo.Usuario
 import br.com.kotlinforum.services.TopicoServices
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.Arrays
@@ -22,5 +24,11 @@ class TopicoController (private val service: TopicoServices){
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long): Topico {
         return service.buscarPorId(id)
+    }
+
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico){
+        service.cadastrar(topico)
     }
 }
--- src/main/kotlin/br/com/kotlinforum/services/TopicoServices.kt
+++ src/main/kotlin/br/com/kotlinforum/services/TopicoServices.kt
@@ -7,25 +7,7 @@ import org.springframework.stereotype.Service
 import java.util.Arrays
 
 @Service
-class TopicoServices(private var topicos: List<Topico>) {
-
-    init {
-        val topico = Topico(
-            id = 1,
-            titulo = "Dúvidas com Kotlin",
-            mensagem = "Metodos em kotlin",
-            curso = Curso(id = 1, nome = "Kotlin", categoria = "Desenvolvimento"),
-            autor = Usuario(id = 1, nome = "José Nailton", email = "nailton@email.com")
-        )
-        val topico1 = Topico(
-            id = 2,
-            titulo = "Dúvidas com Kotlin e java",
-            mensagem = "Metodos em kotlin igual ao java",
-            curso = Curso(id = 2, nome = "Kotlin", categoria = "Desenvolvimento em kotlin"),
-            autor = Usuario(id = 2, nome = "José Nailton", email = "nailton@email.com")
-        )
-        topicos = Arrays.asList(topico, topico1)
-    }
+class TopicoServices(private var topicos: List<Topico> = ArrayList()) {
 
     fun listar(): List<Topico> {
         return topicos
@@ -33,5 +15,9 @@ class TopicoServices(private var topicos: List<Topico>) {
 
     fun buscarPorId(id: Long): Topico {
         return topicos.stream().filter({t -> t.id == id}).findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
     }
 }
